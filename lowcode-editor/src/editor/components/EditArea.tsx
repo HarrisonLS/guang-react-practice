74c32d8fa110a1389be6e7e41693a957ccc4306f
HarrisonLin <lsfpositivee@163.com>
2024-08-14 23:28:19 +0800

    feat: lowcode json增删改查

---
+++ lowcode-editor/src/editor/components/EditArea.tsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { useComponentsStore } from "../stores/component";
+
+export function EditArea() {
+
+    const {components, addComponent} = useComponentsStore();
+
+    useEffect(()=> {
+        addComponent({
+            id: 222,
+            name: 'Container',
+            props: {},
+            children: []
+        }, 1);
+    }, []);
+
+    return <div>
+        <pre>
+            {
+                JSON.stringify(components, null, 2)
+            }
+        </pre>
+    </div>
+}

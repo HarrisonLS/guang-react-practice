b1390e790740808dddc9ed56efaf614d63a45bb5
harrison <lsfpositivee@163.com>
2024-08-16 18:11:32 +0800

    feat: lowcode component hover info

--- lowcode-editor/src/editor/components/EditArea.tsx
+++ lowcode-editor/src/editor/components/EditArea.tsx
@@ -1,30 +1,15 @@
-import { useEffect } from "react";
+import { MouseEventHandler, useEffect, useState } from "react";
 import { Component, useComponentsStore } from "../stores/component";
 import { useComponentConfigStore } from "../stores/component-config";
 import React from "react";
+import HoverMask from "./HoverMask";
 
 export function EditArea() {
 
     const { components, addComponent } = useComponentsStore();
     const { componentConfig } = useComponentConfigStore()
 
-    // useEffect(() => {
-    //     addComponent({
-    //         id: 222,
-    //         name: 'Container',
-    //         props: {},
-    //         children: []
-    //     }, 1);
-
-    //     addComponent({
-    //         id: 333,
-    //         name: 'Button',
-    //         props: {
-    //             text: '无敌'
-    //         },
-    //         children: []
-    //     }, 222);
-    // }, []);
+    const [hoverComponentId, setHoverComponentId] = useState<number>()
 
     function renderComponents(components: Component[]): React.ReactNode {
         return components.map((component: Component) => {
@@ -48,10 +33,38 @@ export function EditArea() {
         })
     }
 
-    return <div className="h-[100%]">
+    const handleMouseOver: MouseEventHandler = (e) => {
+        // 返回当事件被触发时，从事件触发的最内层节点一直到事件捕获的最外层节点的完整事件处理路径
+        const path = e.nativeEvent.composedPath()
+        console.log('path: ', path);
+
+        for (let i = 0; i < path.length; i += 1) {
+            const ele = path[i] as HTMLElement
+
+            // ele.dataset是一个 dom 的属性，包含所有 data-xx 的属性的值
+            const componentId = ele.dataset?.componentId
+            if (componentId) {
+                setHoverComponentId(+componentId)
+                return;
+            }
+        }
+    }
+
+    return <div className="h-[100%] edit-area" onMouseOver={handleMouseOver} onMouseLeave={() => {
+        setHoverComponentId(undefined);
+    }}
+    >
         {/* <pre>
             {JSON.stringify(components, null, 2)}
         </pre> */}
         {renderComponents(components)}
+        {hoverComponentId && (
+            <HoverMask
+                portalWrapperClassName='portal-wrapper'
+                containerClassName='edit-area'
+                componentId={hoverComponentId}
+            />
+        )}
+        <div className="portal-wrapper"></div>
     </div>
 }
